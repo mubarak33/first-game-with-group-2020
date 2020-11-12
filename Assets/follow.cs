3f6e126a0c15056d0f67eeb2b2b6d287c18f7f69
mubarak33 <kuw1955@gmail.com>
2020-11-12 05:58:53 +0000

    first drift upload 12-10-2020

---
+++ Assets/follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+
+    [SerializeField] //for explicity sake
+    public GameObject position_camera; //assign this in the editor or find it in the start function.
+    public GameObject player;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    //private void Start(){
+    //    
+    //}
+
+    void LateUpdate()
+    {
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, position_camera.transform.position, ref velocity, smoothTime);
+        //change the last value to the time you want to complete the movement.
+         transform.LookAt(player.transform);
+    }
+}
